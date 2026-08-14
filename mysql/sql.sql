21bae56f700422ea2b95a61094788c6c2f49aee8
nguyenhongphu27 <phu272007@gmail.com>
2026-08-14 20:01:25 +0700

    buoi1

---
+++ mysql/sql.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS rw_102;
+CREATE DATABASE rw_102;
+USE rw_102;
+DROP TABLE IF EXISTS department;
+CREATE TABLE department(
+   department_id		INT,
+   department_name 		VARCHAR(50)
+);
+
+-- user: id, fullname, user_name, brith_of_date, gender
+(
+	id 				INT,CREATE TABLE user
+	full_name 		VARCHAR(50),
+	birth_of_date 	DATE,
+	gender 			ENUM('male','female'),
+	username 		VARCHAR(50)
+);
+
+SELECT *
+FROM rw_102;
+
+
+
+
+INSERT INTO department(department_id, department_name)
+VALUES (1,'sale'),
+		(2,'marketing'),
+		(3,'bao ve');
+
+DROP DATABASE IF EXISTS Position;
+CREATE DATABASE Position;
+
